c3dab678626946c4c21a0d5bf75141df8972208f
Erik Khalimov <biomaks@gmail.com>
2019-08-12 18:13:00 -0500

    fix region

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 bucket_name = os.getenv("BUCKET_NAME")
 sender = os.getenv("EMAIL_SENDER")
 recipient = os.getenv("EMAIL_RECIPIENT")
+region = os.getenv("AWS_REGION")
--- scraper.py
+++ scraper.py
@@ -1,12 +1,12 @@
 import hashlib
+import boto3
 from datetime import datetime
 
-import boto3
 from botocore.exceptions import ClientError
 from selenium import webdriver
 
 from config import bucket_name as bk_name
-from config import sender, recipient
+from config import sender, recipient, region
 
 url = "http://toronto.kdmid.ru/ru.aspx?lst=ru&it=/%D0%92%D1%8B%D0%B5%D0%B7%D0%B4%D0%BD%D0%BE%D0%B5%20%D0%BA%D0%BE%D0%BD%D1%81%D1%83%D0%BB%D1%8C%D1%81%D0%BA%D0%BE%D0%B5%20%D0%BE%D0%B1%D1%81%D0%BB%D1%83%D0%B6%D0%B8%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5.aspx"
 
@@ -91,7 +91,7 @@ class EmailSender:
         CHARSET = "UTF-8"
 
         # Create a new SES resource and specify a region.
-        client = boto3.client('ses')
+        client = boto3.client('ses', region_name=region)
 
         # Try to send the email.
         try:
